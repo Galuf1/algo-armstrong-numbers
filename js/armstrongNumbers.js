5c44a4e576463b83e5e626b7e2a3b48d5b75f814
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 01:17:16 +0200

    finally solved with reduce

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -8,22 +8,16 @@ let findArmstrongNumbers = function(array) {
     let ans = []
     for (let j = 0; j < array.length; j++) {
         let digits = array[j].toString().length
-        let sum = 0
-        // let digits = 3
-        // console.log(digits)
-        // let thresevenone = 371
-        // let number1 = String(thresevenone).split("")
-        for (let k= 0; k < .length; k++) {
-            num
-        }
+        let number1 = String(array[j]).split("")
 
-        // let number = number1.reduce((a,b) => a**digits+b**digits)
-        // console.log(number,digits)
+        // need to convert to int
+        let number = number1.reduce((a,b) => (parseInt(a))+(parseInt(b)**digits),0)
+        // console.log(number1)
             if (number == array[j]) {
                 ans.push(array[j])
             }
         }
-        // console.log(ans)        
+        console.log(ans)        
     }
     
     
