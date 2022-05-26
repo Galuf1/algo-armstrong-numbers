2ba162fb7d7bf1cd4f676b68b3d0849facb919de
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 00:01:05 +0200

    not working

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,34 @@
-// How can you make this more scalable and reusable later?
+let array = []
+for (let i = 0; i < 1000; i++) {
+    array.push(i)
+}
 
-exports.findArmstrongNumbers = function() {
 
-};
+let findArmstrongNumbers = function(array) {
+    let ans = []
+    for (let j = 0; j < array.length; j++) {
+        let digits = array[j].toString().length
+        let sum = 0
+        // let digits = 3
+        // console.log(digits)
+        // let thresevenone = 371
+        // let number1 = String(thresevenone).split("")
+        for (let k= 0; k < .length; k++) {
+            num
+        }
+
+        // let number = number1.reduce((a,b) => a**digits+b**digits)
+        // console.log(number,digits)
+            if (number == array[j]) {
+                ans.push(array[j])
+            }
+        }
+        // console.log(ans)        
+    }
+    
+    
+
+findArmstrongNumbers(array)
+  
+
+
